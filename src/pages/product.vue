24ace7bfe43ee350d3af961016075b383ecc13cd
AhogeK <ahogek@gmail.com>
2021-01-03 21:34:17 +0800

    产品站页面 产品站参数组建实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    product 
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <button class="btn">
+          立即购买
+        </button>
+      </template>
+    </product-param>
   </div>
 </template>
-
 <script>
+import ProductParam from './../components/ProductParam'
+
 export default {
-    name: 'Product'
+  name: 'Product',
+  components: {
+    ProductParam
+  }
 }
 </script>
+<style lang="scss">
+  .product {
+    button {
+      margin-left: 10px;
+    }
+  }
+</style>
