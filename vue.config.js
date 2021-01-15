5f7696fe94fb2f203a2085e86d8931276eda7cc2
AhogeK <ahogek@gmail.com>
2021-01-15 15:37:23 +0800

    fix eslint

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,20 @@
-export const devServer = {
-	host: 'localhost',
-	port: 8080,
-	proxy: {
-		'/api': {
-			target: 'http://mall-pre.springboot.cn',
-			changeOrigin: true,
-			pathRewrite: {
-				'/api': ''
+module.exports = {
+	devServer: {
+		host: 'localhost',
+		port: 8080,
+		proxy: {
+			'/api': {
+				target: 'http://mall-pre.springboot.cn',
+				changeOrigin: true,
+				pathRewrite: {
+					'/api': ''
+				}
 			}
+		},
+		overlay: {
+			warnings: true,
+			errors: true
 		}
 	},
-	lintOnSave: true,
-	overlay: {
-		warnings: true,
-		errors: true
-	}
-};
+	lintOnSave: true
+}
